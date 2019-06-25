cb73ee67bb5de8d388eb2af5acec761cd8e1d2e6
Artimus <art.rijos@yahoo.com>
2019-06-25 06:40:17 -0400

    Moved Swagger UI.

--- src/main/java/org/arj/jaxrs/springboot/auth/jwt/AppConfig.java
+++ src/main/java/org/arj/jaxrs/springboot/auth/jwt/AppConfig.java
@@ -5,15 +5,37 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.holonplatform.auth.Account;
+import com.holonplatform.auth.Account.AccountProvider;
 import com.holonplatform.auth.AuthenticationToken;
 import com.holonplatform.auth.Realm;
-import com.holonplatform.auth.Account.AccountProvider;
 import com.holonplatform.auth.jwt.JwtAuthenticator;
 import com.holonplatform.auth.jwt.JwtConfiguration;
 
 @Configuration
 @EnableAutoConfiguration
 public class AppConfig {
+//	@Bean
+//    public WebMvcConfigurerAdapter webMvcConfigurerAdapter()
+//    {
+//        return new WebMvcConfigurerAdapter()
+//        {
+//            @Override
+//            public void addResourceHandlers(ResourceHandlerRegistry registry)
+//            {
+//                if (!registry.hasMappingForPattern("/**"))
+//                {
+//                    // if this is executed spring won't add default resource
+//                    // locations - add them to the staticContentLocations if
+//                    // you want to keep them
+//                    // default locations:
+//                    // WebMvcAutoConfiguration.RESOURCE_LOCATIONS
+//                    registry.addResourceHandler("/**").addResourceLocations(
+//                            staticContentLocations);
+//                }
+//            }
+//        };
+//    }
+	
 	@Bean
 	public Realm realm(AccountProvider accountProvider, JwtConfiguration jwtConfiguration) {
 		return Realm.builder()
